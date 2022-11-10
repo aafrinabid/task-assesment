38d17cf656ca041db7d8981649115b95215738ed
aafrinabid <mohdaafrin@gmail.com>
2022-11-10 12:41:42 +0530

    auth service testing is done

--- src/tasks/task.service.spec.ts
+++ src/tasks/task.service.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { TasksRepository } from './tasks.repository';
 import { TasksService } from "./tasks.service";
 
 describe('TestService', () => {
   let service: TasksService;
-
+  let mockTaskRepository = TasksRepository
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [TasksService],
     }).compile();
+    
 
     service = module.get<TasksService>(TasksService);
   });
@@ -17,8 +20,10 @@ describe('TestService', () => {
     expect(service).toBeDefined();
   });
 
-//   it('should get all task',async()=>{
-//     let result:Promise<Task[]>
-//     expect(await service.findAllTasks()).toBe(result)
-//   })
+  it('should get all task',async()=>{
+    let result
+    result = await mockTaskRepository.findAllTask()
+    expect(service.findAllTasks()).toBe(result)
+
+  })
 });
