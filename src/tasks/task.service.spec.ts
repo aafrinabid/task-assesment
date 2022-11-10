d67637ea36119f2c3fd83969d721f58bd3fa0686
aafrinabid <mohdaafrin@gmail.com>
2022-11-10 13:54:18 +0530

    testing done on task services

--- src/tasks/task.service.spec.ts
+++ src/tasks/task.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { TaskStatus } from './task.model';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from "./tasks.service";
 
@@ -21,9 +22,30 @@ describe('TestService', () => {
   });
 
   it('should get all task',async()=>{
-    let result
-    result = await mockTaskRepository.findAllTask()
-    expect(service.findAllTasks()).toBe(result)
+    let result:Promise<Task[]>
+    jest.spyOn(mockTaskRepository,'getAllTask').mockImplementation(()=>result)
+    expect(await service.getAllTask({user:{id:1}})).toBe(result)
+    
+  })
+  it('get task by Id',async()=>{
+    let result:Promise<Task>
+    jest.spyOn(mockTaskRepository,'getTasksById').mockImplementation(()=>result)
+    expect(await service.getTaskById(1)).toBe(result)
+  })
+  it('create task',async ()=>{
+    let result:Promise<Task>
+    jest.spyOn(mockTaskRepository,'createTask').mockImplementation(()=>result)
+    expect(await service.createTask({title:'groceries',description:'buy apple'},1)).toBe(result)
+  })
+  it('update a task',async()=>{
+    let result:Promise<Task>
+    jest.spyOn(mockTaskRepository,'updateTaskStatus').mockImplementation(()=>result)
+    expect(await service.updateTaskStatus(1,TaskStatus.DONE)).toBe(result)
+  })
+  it('delete a task',async ()=>{
+    let result:Promise<void>
+    jest.spyOn(mockTaskRepository,'delteTask').mockImplementation(()=>result)
+    expect(await service.deleteTask(1)).toBe(result)
 
   })
 });
